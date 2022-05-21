40334b83724dd5ed7bbb0d41ddc2ae2ea0cd1087
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-05-21 08:52:27 -0400

    Make adjustments to styles for CTA layout

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -4,11 +4,13 @@ import "./CallToAction.css";
 const CallToAction = ({ scrollToTop }) => {
   return (
     <section className="cta">
-      <h2 className="section-heading">Let’s talk about your project</h2>
-      <p className="paragraph">
-        Ready to take it to the next level? Contact us today and find out how
-        our expertise can help your business grow.
-      </p>
+      <div>
+        <h2 className="section-heading">Let’s talk about your project</h2>
+        <p className="paragraph">
+          Ready to take it to the next level? Contact us today and find out how
+          our expertise can help your business grow.
+        </p>
+      </div>
       <Link to="/contact" className="btn btn-light" onClick={scrollToTop}>
         Get in touch
       </Link>
